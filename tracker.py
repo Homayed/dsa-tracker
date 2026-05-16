64a48f6b9cda2a44dbfb563e913b639243593ead
Homayed <naser154439@gmail.com>
2026-05-16 16:06:10 +0100

    updated

--- tracker.py
+++ tracker.py
@@ -8,6 +8,11 @@ def add_problem():
     difficulty = input("difficulty(Easy/Medium/hard): ")
     time_taken = int(input("Time Taken(minutes): "))
     solved_alone = input("Solved alone?(Yes/No): ")
+    pattern = input("Whats the Pattern?")
+    time_complexity = input("Whats the time complexity of the problem")
+    space_complexity = input("whats the space complexity of the problem")
+    note_file = input("Note file path: ")
+
 #dict
     entry = {
         "date": str(datetime.now().date()),
@@ -15,9 +20,15 @@ def add_problem():
         "topic": topic,
         "difficulty": difficulty,
         "time_taken": time_taken,
-        "solved_alone": solved_alone
+        "solved_alone": solved_alone,
+        "pattern": pattern,
+        "time_complexity": time_complexity,
+        "space_complexity": space_complexity,
+        "note_file": note_file
+
     }
     data.append(entry)#add new entry
+
     save_data(data) #write in JSON
     print("\nProblem added successfully.\n")
 
@@ -28,7 +39,10 @@ def view_all_problems():
         return
     print("\n DSA Practice Log:\n")
     for i, item in enumerate(data,1): #loop through index and value where index starts from 1
-        print(f"{i}.{item['date']}| {item['problem']} | {item['topic']} | {item['difficulty']} | {item['time_taken']} | {item['solved_alone']}")
+        print(f"{i}.{item['date']}| {item['problem']} | {item['topic']} | {item['difficulty']} | {item['time_taken']} | {item['solved_alone']} | {item.get('pattern', 'Not added yet')} | {item.get('time_complexity', 'Not added yet')}| {item.get('space_complexity', 'Not added yet')} | {item.get('note_file', 'Not added yet')}")
+
+
+
 
 def delete_problem():
     data = load_data()
@@ -49,7 +63,7 @@ def search_problem():
 
     for item in data:
         if "problem" in item:
-            if item["problem"].lower() == searching.lower():
+            if item["problem"].lower().strip() == searching.lower().strip():
                 print(item)
                 found = True
 
